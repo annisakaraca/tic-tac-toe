3b5f473c01335a791e9b79b267c19af32e5de9ba
Annisa <annisakaraca@gmail.com>
2018-07-18 14:09:36 -0700

    Integrate readline to get player input

--- game.js
+++ game.js
@@ -1,1 +1,45 @@
-console.log('Welcome to the new game! Where would you like to place your piece?')
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> '
+});
+
+var boardState = [
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0]
+];
+
+console.log('Welcome to the new game!')
+var piecesPlaced = 0;
+
+// while (piecesPlaced < 10) {
+//   rl.pause();
+
+//   rl.question('Player 1: Where would you like to place your piece?', (answer) => {
+//     // update board state
+  
+//     // if player selects unavailable square, ask again
+  
+//     // check for winner
+
+//     rl.resume();
+//   });
+
+// }
+
+rl.prompt();
+rl.on('line', (input) => {
+  // update board state
+
+  // if player selects unavailable square ask again
+
+  // check for winner
+
+
+  console.log('input: ', input)
+});
+
+
